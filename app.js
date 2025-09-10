65d64dd611d8dee0f64ae67fafb7de181d73a0db
Yasir Gaji <hellopleasantcodes@gmail.com>
2025-09-10 08:32:44 +0100

    setup home page

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const port = 3003;
 
 const server = app.listen(port, () => console.log('yo ' + port));
 
-app.set("view engine", "pug")
+app.set("view engine", "pug");
+app.set("views", "views");
 
 app.get("/", (req, res, next) => {
-  res.status(200).send("wire")
+  
+  var payload = {
+    pageTitle: 'Home'
+  }
+
+
+  res.status(200).render("home", payload)
 })
