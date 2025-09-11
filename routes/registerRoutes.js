6b068fdc6568b906642d2b29ba7b9d67ad1cfd17
Yasir Gaji <hellopleasantcodes@gmail.com>
2025-09-11 18:04:02 +0100

    worked on server side validation for empty fields checking

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const bodyParser = require("body-parser")
-
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -14,9 +13,19 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+  var firstName = req.body.firstName.trim();
+  var lastName = req.body.lastName.trim();
+  var username = req.body.username.trim();
+  var email = req.body.email.trim();
+  var password = req.body.password;
 
-  console.log(req.body);
-  res.status(200).render('register');
+  var payload = req.body;
+
+  if (firstName && lastName && username && email && password) {
+  } else {
+    payload.errorMessage = "Make sure each field has a valid value."
+    res.status(200).render('register', payload);
+  }
 });
 
 module.exports = router;
